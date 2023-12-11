73ba23225f20cbe8c4402288e998acd6da032b50
Luc Hayward <luchay10@gmail.com>
2023-12-11 19:46:47 +0200

    Finish day 2

--- 1/main.go
+++ 1/main.go
@@ -7,20 +7,31 @@ import (
 	"strconv"
 )
 
-// Lines of text contain calibration values
-// Each line originally contained a **calibration value** that must be recovered.
-// Combine the first digit and the last digit to make a 2-digit number
-// Eg:
-// 1abc2 => 12
-// pqr3stu8vwx -> 38
-// a1b2c3d4e5f -> 15
-// treb7uchet -> 77
-// Find the sum of these values => 142
+/*
+	Lines of text contain calibration values
+
+Each line originally contained a **calibration value** that must be recovered.
+Combine the first digit and the last digit to make a 2-digit number
+Eg:
+1abc2 => 12
+pqr3stu8vwx -> 38
+a1b2c3d4e5f -> 15
+treb7uchet -> 77
+Find the sum of these values => 142
+
+Benchmark 1: go run main.go
+Time (mean ± σ):     353.9 ms ±  22.2 ms    [User: 196.7 ms, System: 187.6 ms]
+Range (min … max):   335.0 ms … 410.4 ms    10 runs
+
+Benchmark 1: ./main
+Time (mean ± σ):      11.4 ms ±  29.5 ms    [User: 1.4 ms, System: 1.6 ms]
+Range (min … max):     0.0 ms … 120.7 ms    16 runs
+*/
 func main() {
 	// Read in a file
 	//testData := [4]string{"1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"}
 
-	file, err := os.Open("1/input.txt")
+	file, err := os.Open("input.txt")
 	check(err)
 	defer file.Close()
 
--- 2/main.go
+++ 2/main.go
@@ -1,5 +1,81 @@
 package main
 
-func main() {
+import (
+	_ "embed"
+	"fmt"
+	"strconv"
+	"strings"
+)
 
+//go:embed input.txt
+var inputTxt string
+
+/*
+Bag of cubes -> RGB
+Play a game:
+- Hide N cubes of each colour (Nr, Ng, Nb) in the bag
+- Elf shows you K random cubes J times
+*/
+func main() {
+	lines := strings.Split(inputTxt, "\n")
+
+	// part1(lines)
+	part2(lines)
+}
+
+// In Part2:
+// Determine the _fewest number of cubes of each colour_ that could have been in the bag
+// ie: Given all the groups find max([rgb])
+func part2(lines []string) {
+	powerSum := 0
+	for _, line := range lines {
+		cubes := strings.Split(line, " ")[2:]
+
+		maxRed, maxGreen, maxBlue := 0, 0, 0
+		for i := 0; i < len(cubes); i += 2 { // iterate over the game 2 at a time for (num, colour)
+			num, _ := strconv.Atoi(cubes[i])
+			colour := rune(cubes[i+1][0])
+
+			switch colour {
+			case 'r':
+				maxRed = max(maxRed, num)
+			case 'g':
+				maxGreen = max(maxGreen, num)
+			case 'b':
+				maxBlue = max(maxBlue, num)
+			}
+		}
+		powerSum += maxRed * maxGreen * maxBlue
+
+	}
+	fmt.Println(powerSum)
+}
+
+// Given many games, determine which games would have been possible if the bag contained only 12R, 13G, 14B
+// ie: determine which games R<=12, G<=13, B<=14
+// Add the IDs of all these games
+func part1(lines []string) {
+	var goodIdsCnt int = 0
+	for game, line := range lines {
+		game = game
+		cubes := strings.Split(line, " ")
+		id, _ := strconv.Atoi(cubes[1][:len(cubes[1])-1])
+		cubes = cubes[2:]
+
+		goodGame := true
+		for i := 0; i < len(cubes); i += 2 {
+			num, _ := strconv.Atoi(cubes[i])
+			colour := rune(cubes[i+1][0])
+
+			if (num > 12 && colour == 'r') || (num > 13 && colour == 'g') || (num > 14 && colour == 'b') {
+				goodGame = false
+				fmt.Println(id)
+				break
+			}
+		}
+		if goodGame {
+			goodIdsCnt += id
+		}
+	}
+	fmt.Println(goodIdsCnt)
 }
